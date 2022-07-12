fb124df2feb0cf65d34e8e9401fc58502b57b9fe
o0m4r <omarksm09@gmail.com>
2022-07-12 02:13:04 +0200

    init commit, templates version

---
+++ app/routes.py
@@ -0,0 +1,20 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+def route():
+    return "hello anyone"
+@app.route('/index')
+def index():
+    user = {'username':'Omar'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html',user=user, posts=posts)
